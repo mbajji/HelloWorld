18672055ca85299571783df9908e2f250e442b80
mbajji <43502655+mbajji@users.noreply.github.com>
2018-09-22 12:35:35 -0700

    Java class codes

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+  public static void main (String [] args) {
+    String codingClass = "Hello World";
+    System.out.println(codingClass);
+    
+    int number =33;
+    int number2 = 55;
+    System.out.println((double)number/number2);
+    double num = 1.0;
+    double num2 = 9.0;
+    System.out.println(num/num2);
+  }
+}
